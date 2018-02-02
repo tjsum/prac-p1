2e45868b62717478156d43b8e1edebe9c13bf8c5
suman <suman_2220@yahoo.com>
2018-02-02 09:32:39 +0530

    second time changes made on App.java

--- src/main/java/com/prac/prac_p1/App.java
+++ src/main/java/com/prac/prac_p1/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello suman!" );
+        System.out.println( "Hello suman " );
+        System.out.println("Good morning have a nice day");
     }
 }
